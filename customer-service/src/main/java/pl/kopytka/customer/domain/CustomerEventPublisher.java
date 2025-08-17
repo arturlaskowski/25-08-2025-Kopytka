f067f0f169a31cb6bde13336c9c126d12374da11
Artur Laskowski <artur.laskowski@comarch.com>
2025-08-17 22:08:22 +0200

    eda-start

---
+++ customer-service/src/main/java/pl/kopytka/customer/domain/CustomerEventPublisher.java
@@ -0,0 +1,7 @@
+package pl.kopytka.customer.domain;
+
+import pl.kopytka.common.domain.event.DomainEventPublisher;
+import pl.kopytka.customer.domain.event.CustomerEvent;
+
+public interface CustomerEventPublisher extends DomainEventPublisher<CustomerEvent> {
+}
--- customer-service/src/main/java/pl/kopytka/customer/messaging/KafkaCustomerEventPublisher.java
+++ customer-service/src/main/java/pl/kopytka/customer/messaging/KafkaCustomerEventPublisher.java
@@ -4,22 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.kopytka.avro.customer.CustomerEventAvroModel;
 import pl.kopytka.avro.customer.CustomerEventType;
-import pl.kopytka.common.domain.event.DomainEventPublisher;
 import pl.kopytka.common.kafka.producer.KafkaProducer;
-import pl.kopytka.customer.domain.event.CustomerCreatedEvent;
+import pl.kopytka.customer.domain.CustomerEventPublisher;
+import pl.kopytka.customer.domain.event.CustomerEvent;
 
 @Component
 @RequiredArgsConstructor
-public class CustomerCreatedEventPublisher implements DomainEventPublisher<CustomerCreatedEvent> {
+class KafkaCustomerEventPublisher implements CustomerEventPublisher {
 
     private final TopicsConfigData topicsConfigData;
     private final KafkaProducer<String, CustomerEventAvroModel> kafkaProducer;
 
     @Override
-    public void publish(CustomerCreatedEvent domainEvent) {
-        var customerId = domainEvent.getCustomer().getCustomerId().id();
+    public void publish(CustomerEvent customerEvent) {
+        var customerId = customerEvent.getCustomer().getCustomerId().id();
         var customerEventAvroModel = new CustomerEventAvroModel(customerId,
-                CustomerEventType.CUSTOMER_CREATED, domainEvent.getCreatedAt());
+                CustomerEventType.CUSTOMER_CREATED, customerEvent.getCreatedAt());
 
         kafkaProducer.send(topicsConfigData.getCustomerEvent(),
                 customerId.toString(),
